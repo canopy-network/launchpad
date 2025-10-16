df4e8d79af9c097e1d3f5d452e629a8e56740ab8
Eric Nielson <ezeike@gmail.com>
2025-10-16 19:53:41 -0400

    removed migrations

--- migrations/20250928202412_modify_template_schema.sql
+++
@@ -1,4 +0,0 @@
--- Modify "chain_templates" table
-ALTER TABLE "chain_templates" DROP COLUMN "default_modules", DROP COLUMN "required_modules";
--- Modify "chains" table
-ALTER TABLE "chains" ALTER COLUMN "template_id" DROP NOT NULL;
